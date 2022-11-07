a31e4771d08b5f3ca40364421ee7b3d1ca6b33da
Allam05 <xyzalan3030@gmail.com>
2022-11-07 14:55:31 -0600

    Commit inicial

---
+++ SDGControlesWebIU/SControlesWebIU.vb
@@ -0,0 +1,4 @@
+﻿
+Public Class SControlesWebIU
+
+End Class
---
+++ SDGControlesWebIU/SExtensiones.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Web.UI
+
+Public Module SExtensiones
+    <Extension()>
+    Public Function ObtenerControles(Of T As Control)(ByRef contenedor As Control) As List(Of T)
+        Dim controles As List(Of T) = New List(Of T)
+        For Each c As Control In contenedor.Controls
+            If TypeOf c Is T Then
+                controles.Add(CType(c, T))
+            End If
+
+            controles.AddRange(ObtenerControles(Of T)(c))
+        Next
+
+        Return controles
+    End Function
+End Module
