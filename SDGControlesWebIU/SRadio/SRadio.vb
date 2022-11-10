5648812c82ae87a968b091429c0c6a286cbfa4ca
Allam05 <xyzalan3030@gmail.com>
2022-11-10 14:46:44 -0600

    Añadiendo RadioButtons, falta hacer que compile especialmente los radios

---
+++ SDGControlesWebIU/SRadio/SRadio.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Web.UI
+''' <summary>
+''' Componente de Radio botón personalizado
+''' </summary>
+<ToolboxData("<{0}:SRadio ID=srd_ runat=server />")>
+Public Class SRadio
+    Inherits System.Web.UI.WebControls.RadioButton
+
+    Sub New()
+        InputAttributes.Add("class", "form-check-input")
+        LabelAttributes.Add("class", "form-check-label")
+    End Sub
+
+    Protected Overrides Sub OnPreRender(e As EventArgs)
+        MyBase.OnPreRender(e)
+        If Not Enabled Then
+            InputAttributes.Add("disabled", "")
+        Else
+            InputAttributes.Remove("disabled")
+        End If
+    End Sub
+
+    Public Overrides Sub RenderControl(writer As HtmlTextWriter)
+        writer.Write("<div class='form-check'>")
+        MyBase.RenderControl(writer)
+        writer.Write("</div>")
+    End Sub
+
+End Class
---
+++ SDGControlesWebIU/SRadio/SRadioLista.vb
@@ -0,0 +1,45 @@
+﻿Imports System.ComponentModel
+Imports System.Web.UI
+''' <summary>
+''' Contenedor de radio buttons
+''' </summary>
+<ToolboxData("<{0}:SRadioLista ID=srl_ runat=server></{0}:SRadioLista>")>
+Public Class SRadioLista
+    Inherits System.Web.UI.WebControls.RadioButtonList
+
+    Sub New()
+        EnableViewState = True
+    End Sub
+
+    ''' <summary>
+    ''' Este texto va a generar un label en el contenedor
+    ''' </summary>
+    ''' <returns></returns>
+    <DefaultValue("SRadioLista")>
+    <Category("Datos")>
+    <Localizable(True)>
+    Public Property Titulo As String
+        Get
+            Return IIf(ViewState("titulo_sradiolista") <> Nothing, ViewState("titulo_sradiolista"), "")
+        End Get
+        Set(value As String)
+            ViewState("titulo_sradiolista") = value
+        End Set
+    End Property
+
+    Public Overrides Sub RenderBeginTag(writer As HtmlTextWriter)
+        Dim estaHabilitado = ""
+        If Not Enabled Then
+            estaHabilitado = "disabled"
+        End If
+
+        writer.Write($"<fieldset {estaHabilitado}>")
+        If (Titulo.Length > 0) Then
+            writer.Write($"<legend>{Titulo}</legend>")
+        End If
+    End Sub
+
+    Public Overrides Sub RenderEndTag(writer As HtmlTextWriter)
+        writer.Write("</fieldset>")
+    End Sub
+End Class
